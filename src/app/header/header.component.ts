8fd55254c710804de58c99e4c807f9fd949f777a
NguyenKhanh <nguyenkhanh19001069@gmail.com>
2023-04-29 01:52:15 +0700

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  
+  constructor(private router: Router, private auth: AuthService) {}
+  
+  ngOnInit(): void {}
+
+  goToHome() {
+    this.router.navigate(['home']);
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnInit{
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login(userName: string, password: string) {
+    if (userName === 'abaron' && password === '123456') {
+      return 200;
+    }
+    else {
+      return 403;
+    }
+  }
+
+  logout() {
+    this.router.navigate(['login']);
+  }
 }
